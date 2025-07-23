bf8aea3aeba0d6f02091c63c6ccf16594a7e85a6
DREADANGEL777 <dreadangel27@gmail.com>
2025-07-23 20:03:39 +0300

    first com

--- src/test.jsx
+++ src/test.jsx
@@ -3,13 +3,7 @@ import React from "react"
 const test = () => {
   return (
     <div>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
+
     </div>
   )
 }
